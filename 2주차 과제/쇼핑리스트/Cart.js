7eee90f2cb5fc8797deca6c215547f8c5569784c
se0jinYoon <tjwls1999@gmail.com>
2024-04-18 21:53:39 +0900

    feat: 장바구니 테이블 뷰 구현

--- 2주차 과제/쇼핑리스트/Cart.js
+++ 2주차 과제/쇼핑리스트/Cart.js
@@ -1,3 +1,28 @@
+// session storage 값 가져오기
 const cartList = sessionStorage.getItem('cartList') ? JSON.parse(sessionStorage.getItem('cartList')) : [];
 
 console.log(cartList);
+
+// 장바구니 table html
+const cartTableBody = document.querySelector('.cart-list__body');
+
+const createCartTableItem = (cartItem) => {
+  return `
+    <tr class="cart-list__tr">
+      <td class="cart-list__td">
+        <input type="checkbox" class="cart-check" />
+      </td>
+      <td class="cart-list__td">
+        <div class="cart-list__img-wrapper">
+          <img src=${cartItem.imgUrl} alt=${cartItem.imgAlt} />
+        </div>
+      </td>
+      <td class="cart-list__td">${cartItem.name}</td>
+      <td class="cart-list__td">${cartItem.price}</td>
+      <td class="cart-list__td">${cartItem.category}</td>
+      <td class="cart-list__td">
+        <button class="cart-list__remove-btn" type="button">삭제</button>
+      </td>
+    </tr>
+  `
+}
