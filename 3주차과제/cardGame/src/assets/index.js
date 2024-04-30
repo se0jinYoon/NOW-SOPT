b3ad22e587ed45ac238bad112dc6b039625cb045
se0jinYoon <tjwls1999@gmail.com>
2024-05-01 02:41:59 +0900

    feat: 상수데이터 저장

---
+++ 3주차과제/cardGame/src/assets/index.js
@@ -0,0 +1,14 @@
+const GAME_DATA = [
+  { id: 0, name: '메뉴판건휘', src: '/src/assets/img/web_img_1.jpeg' },
+  { id: 1, name: '새싹들', src: '/src/assets/img/web_img_2.jpeg' },
+  { id: 2, name: '누구게', src: '/src/assets/img/web_img_3.jpeg' },
+  { id: 3, name: '쿨쿨당니', src: '/src/assets/img/web_img_4.jpeg' },
+  { id: 4, name: '쿨쿨재훈', src: '/src/assets/img/web_img_5.jpeg' },
+  { id: 5, name: '토끼정안', src: '/src/assets/img/web_img_6.jpeg' },
+  { id: 6, name: '정우또가', src: '/src/assets/img/web_img_7.jpeg' },
+  { id: 7, name: '주용태승', src: '/src/assets/img/web_img_8.jpeg' },
+  { id: 8, name: '원탁회의', src: '/src/assets/img/web_img_9.jpeg' },
+  { id: 9, name: '마일사랑해', src: '/src/assets/img/web_img_10.jpeg' },
+];
+
+export default GAME_DATA;
